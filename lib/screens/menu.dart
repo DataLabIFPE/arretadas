421118cedf6c51d85077176f3f5e6097d03fcd4f
Victor-Raphael <56162733+victoorraphael@users.noreply.github.com>
2020-09-02 14:47:53 -0300

    send phones to API NODE

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,6 +1,6 @@
+import 'package:arretadas/components/Button.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'alerta.dart';
+// import 'package:geolocator/geolocator.dart';
 
 class Menu extends StatefulWidget {
   @override
@@ -8,11 +8,11 @@ class Menu extends StatefulWidget {
 }
 
 class _MenuState extends State<Menu> {
-  void _getLocation() async {
-    final location = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    print(location);
-  }
+  // void _getLocation() async {
+  //   final location = await Geolocator()
+  //       .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  //   print(location);
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -72,38 +72,46 @@ class _MenuState extends State<Menu> {
                             child: Image.asset('assets/icon-person.png')),
                       ],
                     ),
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: 90.0,
-                      margin: EdgeInsets.only(left: 6, right: 6, top: 15),
-                      decoration: BoxDecoration(
-                        color: Colors.yellowAccent,
-                        borderRadius: BorderRadius.circular(90),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Icon(
-                            Icons.warning,
-                            size: 60,
-                          ),
-                          FlatButton(
-                            color: Colors.yellowAccent,
-                            textColor: Colors.black,
-                            disabledColor: Colors.grey,
-                            padding: EdgeInsets.all(35.0),
-                            splashColor: Colors.white,
-                            onPressed: () {
-                              _getLocation();
-                              showAlertDialog2(context);
-                            },
-                            child: Text(
-                              "PEDIR SOCORRO",
-                              style: TextStyle(fontSize: 22.0),
-                            ),
-                          ),
-                        ],
-                      ),
+                    // Container(
+                    //   width: MediaQuery.of(context).size.width,
+                    //   height: 90.0,
+                    //   margin: EdgeInsets.only(left: 6, right: 6, top: 15),
+                    //   decoration: BoxDecoration(
+                    //     color: Colors.yellowAccent,
+                    //     borderRadius: BorderRadius.circular(90),
+                    //   ),
+                    //   child: Row(
+                    //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    //     children: <Widget>[
+                    //       Icon(
+                    //         Icons.warning,
+                    //         size: 60,
+                    //       ),
+                    //       FlatButton(
+                    //         color: Colors.yellowAccent,
+                    //         textColor: Colors.black,
+                    //         disabledColor: Colors.grey,
+                    //         padding: EdgeInsets.all(35.0),
+                    //         splashColor: Colors.white,
+                    //         onPressed: () {
+                    //           _getLocation();
+                    //           showAlertDialog2(context);
+                    //         },
+                    //         child: Text(
+                    //           "PEDIR SOCORRO",
+                    //           style: TextStyle(fontSize: 22.0),
+                    //         ),
+                    //       ),
+                    //     ],
+                    //   ),
+                    // ),
+                    Button(
+                      type: 'help',
+                      txtColor: Colors.redAccent,
+                      btnColor: Colors.white,
+                      context: context,
+                      rota: '',
+                      labelText: 'PEDIR SOCORRO',
                     ),
                     //Botão denunciar
                     Container(
