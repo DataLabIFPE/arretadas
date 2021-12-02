c87483542c3eed28909c8d9118bf016db6b2d142
João Victor Lacerda de Queiroz <victorqueiroz90@outlook.com>
2021-12-02 10:50:22 -0300

    Mandando a cidade para o controller na autenticação

--- lib/app/modules/auth/external/datasources/auth_api/auth_api.dart
+++ lib/app/modules/auth/external/datasources/auth_api/auth_api.dart
@@ -2,7 +2,7 @@ import 'package:arretadas/app/modules/auth/domain/entities/user.dart';
 import 'package:arretadas/app/modules/auth/domain/erros/erros.dart';
 import 'package:arretadas/app/modules/auth/domain/usecases/login_usecase.dart';
 import 'package:arretadas/app/modules/auth/infra/datasources/auth_datasource.dart';
-import 'package:arretadas/app/modules/complaints/presenter/pages/ContollerMap/Controller.dart';
+import 'package:arretadas/app/modules/complaints/presenter/pages/ContollerMap/ControllerCity.dart';
 import 'package:dio/dio.dart';
 
 import 'mapper.dart';
@@ -19,8 +19,7 @@ class AuthApi implements AuthDatasource {
           'https://arretadas-api.herokuapp.com/user/authenticate',
           data: {'nickname': params.nickname, 'password': params.password});
 
-      Controller().setCity(response.data['data']['city']);
-
+      ControllerCity().setCity(response.data['data']['city']);
       return UserMapper.fromJson(response.data);
     } on DioError catch (e) {
       throw AuthException(e.message);
