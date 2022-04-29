65570bffafa81b75bd71deb87ea24e47bf0eeae3
Samuel Ferraz <ssf@discente.ifpe.edu.br>
2022-04-29 18:46:11 -0300

    deletions of tests useless

--- test/app/modules/complaints/complaints_module_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_modular_test/flutter_modular_test.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:arretadas/app/modules/complaints/complaints_module.dart';
-
-void main() {
-  setUpAll(() {
-    initModule(ComplaintsModule());
-  });
-}
--- test/app/modules/usefulcontacts/usefulcontacts_module_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_modular_test/flutter_modular_test.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:arretadas/app/modules/usefulcontacts/usefulcontacts_module.dart';
-
-void main() {
-  setUpAll(() {
-    initModule(UsefulcontactsModule());
-  });
-}
