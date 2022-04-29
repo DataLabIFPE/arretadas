82bc890a0a431d8e47d21df8179c826036544672
Samuel Ferraz <ssf@discente.ifpe.edu.br>
2022-04-29 18:39:09 -0300

    fixing imports splash repository

--- lib/app/modules/splash/splash_repository.dart
+++ lib/app/modules/splash/splash_repository.dart
@@ -2,6 +2,8 @@ import 'package:arretadas/app/modules/auth/domain/entities/user.dart';
 import 'package:arretadas/app/modules/auth/external/datasources/auth_api/mapper.dart';
 import 'package:dio/dio.dart';
 
+import '../../core/constants/api_endpoint.dart';
+
 class SplashRepository {
   final Dio dio;
 
@@ -9,13 +11,12 @@ class SplashRepository {
 
   Future<User> refreshToken(User user) async {
     try {
-      final response = await dio.post(
-          "https://arretadas-api.herokuapp.com/user/refresh-token",
-          data: {
-            'token': user.token,
-            'id': user.id,
-            'nickname': user.nickname,
-          });
+      final response =
+          await dio.post("${ApiEndpoint.url_heroku}/user/refresh-token", data: {
+        'token': user.token,
+        'id': user.id,
+        'nickname': user.nickname,
+      });
       return UserMapper.fromJson(response.data);
     } on DioError catch (e) {
       print(e);
